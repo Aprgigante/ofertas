7d35d5c1db296e53c7147cac5380d6dcab6345b1
Aprgigante <goytanet@gmail.com>
2026-07-09 21:10:57 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,361 @@
+//// ======================================
+// APR GIGANTE OFERTAS - VERSÃO 6.0
+// ======================================
+
+// TIPOS:
+// oferta
+// vendido
+// recomendado
+
+const produtos = [
+
+{
+    nome: "Case Adaptador Caddy HD 9,5mm",
+    preco: 15.42,
+    categoria: "ferramentas",
+    tipo: "oferta",
+    destaque: true,
+    imagem: "images/produtos/caddy.jpeg",
+    link: "https://meli.la/1eBqqHv"
+},
+
+{
+    nome: "Parafusadeira Furadeira De Impacto The Black Tools Profissional TB-21PX 2 Baterias Com Maleta 60Hz Amarelo",
+    preco: 168.00,
+    categoria: "ferramentas",
+    tipo: "vendido",
+    destaque: true,
+    imagem: "images/produtos/Parafusadeira Furadeira De Impacto The Black Tools Profissional TB-21PX 2 Baterias Com Maleta 60Hz Amarelo.jpeg",
+    link: "https://meli.la/124W3TA"
+},
+{
+    nome: "Furadeira Industrial Impacto Rev 450w Gsb 450 Re Bosch + Kit",
+    preco: 299.90,
+    categoria: "ferramentas",
+    tipo: "vendido",
+    destaque: true,
+    imagem: "images/produtos/Furadeira Industrial Impacto Rev 450w Gsb 450 Re Bosch + Kit.jpeg",
+    link: "https://meli.la/2NjBU5a"
+},
+{
+    nome: "Trena Digital Laser Até 60 Metros Com Nível Alta Precisão",
+    preco: 119.40,
+    categoria: "ferramentas",
+    tipo: "vendido",
+    destaque: true,
+    imagem: "images/produtos/Trena Digital Laser Até 60 Metros Com Nível Alta Precisão.jpeg",
+    link: "https://meli.la/1QSvQJJ"
+},
+{
+    nome: "Serra Mármore 4.3/8 1300w C/ 2discos 4100NH3ZX2 Makita.jpeg",
+    preco: 330.51,
+    categoria: "ferramentas",
+    tipo: "vendido",
+    destaque: true,
+    imagem: "Images/Produtos/Serra Mármore 4.38 1300w C 2discos 4100NH3ZX2 Makita.jpeg",
+    link: "https://meli.la/18xZoX5"
+},
+{
+    nome: "Alicate Universal 8 Pol Cromo Vanádio Gedore R28301200 1000V Cor Vermelho",
+    preco: 45.00,
+    categoria: "eletrica",
+    tipo: "recomendado",
+    destaque: false,
+    imagem: "images/produtos/Alicate Universal 8 Pol Cromo Vanádio Gedore R28301200 1000V Cor Vermelho.jpeg",
+    link: "https://meli.la/1GoQEGA"
+},
+{
+    nome: "Alicate Universal Isolado Eletricista 8 Polegada Gedore",
+    preco: 109.00,
+    categoria: "eletrica",
+    tipo: "recomendado",
+    destaque: false,
+    imagem: "images/produtos/Alicate Universal Isolado Eletricista 8 Polegada Gedore.jpeg",
+    link: "https://meli.la/15H3ZGF"
+},
+{
+    nome: "Caneta Detectora De Energia Elétrica Teste Tensão Sonora Bip Lanterna Led Luz fios tomadas",
+    preco: 19.00,
+    categoria: "eletrica",
+    tipo: "vendido",
+    destaque: false,
+    imagem: "images/produtos/Caneta Detectora  de Corrente Elétrica.jpeg",
+    link: "https://meli.la/1YuYqts"
+},
+{
+    nome: "Alicate De Bico Reto 8 Gedore Red",
+    preco: 52.90,
+    categoria: "eletrica",
+    tipo: "vendido",
+    destaque: false,
+    imagem: "images/produtos/Alicate De Bico Reto 8 Gedore Red.jpeg",
+    link: "https://meli.la/2h35THr"
+},
+{
+    nome: "Kit 12 Jogo Chaves Isoladas Imã Fenda E Phillips Eletricista",
+    preco: 45.00,
+    categoria: "eletrica",
+    tipo: "vendido",
+    destaque: false,
+    imagem: "images/produtos/Kit Fenda e Philips.jpeg",
+    link: "https://meli.la/1UgDA38"
+},
+{
+    nome: "Alicate Amperímetro Brasfort",
+    preco: 48.50,
+    categoria: "eletrica",
+    tipo: "vendido",
+    destaque: false,
+    imagem: "images/produtos/Alicate Amperímetro Brasfort.jpeg",
+    link: "https://meli.la/1oRPamy"
+},
+{
+    nome: "Guia Passa Fios Eletricista Sonda Ferramenta Com Alma Aco",
+    preco: 29.00,
+    categoria: "eletrica",
+    tipo: "vendido",
+    destaque: false,
+    imagem: "images/produtos/Passa Fio Alma de Aço.jpeg",
+    link: "https://meli.la/2w2Mu73"
+},
+{
+    nome: "Chuveiro Elétrico",
+    preco: 99.00,
+    categoria: "eletrica",
+    tipo: "recomendado",
+    destaque: true,
+    imagem: "images/produtos/Chuveiro Elétrico.jpeg",
+    link: "https://meli.la/2a6mKbW"
+},
+
+{
+    nome: "Jogo De Bits 50 Peças Magnético Ponteira.jpeg",
+    preco: 54.10,
+    categoria: "ferramentas",
+    tipo: "vendido",
+    destaque: false,
+    imagem: "images/produtos/Jogo De Bits 50 Peças Magnético Ponteira.jpeg",
+    link: "https://meli.la/2KfL8V5"
+},
+
+{
+    nome: "Torneira Gourmet Monocomando Cozinha Luxo Mesa Com Mola Super Com Corpo Em Metal Premium Altissima Qualidade",
+    preco: 108.16,
+    categoria: "hidraulica",
+    tipo: "recomendado",
+    destaque: true,
+    imagem: "images/produtos/torneira gourmet.jpeg",
+    link: "https://meli.la/1dPUN1n"
+},
+{
+    nome: "Alicate Pressão Chave Inglesa Grifo Profissional Hidráulica",
+    preco: 65.86,
+    categoria: "hidraulica",
+    tipo: "recomendado",
+    destaque: true,
+    imagem: "images/produtos/Alicate Pressão Chave Inglesa Grifo Profissional Hidráulica.jpeg",
+    link: "https://meli.la/2detZbu"
+},
+ {
+    nome: "Rebarbador Escariador Manual Tubo Cobre Alumínio Acabamento",
+    preco: 25.35,
+    categoria: "hidraulica",
+    tipo: "recomendado",
+    destaque: true,
+    imagem: "images/produtos/Rebarbador Escariador Manual Tubo Cobre Alumínio Acabamento.jpeg",
+    link: "https://meli.la/2z2Zizj"
+},  
+{
+    nome: "Alicatechave Curva Para Instação De Torneiras Encanador",
+    preco: 65.90,
+    categoria: "hidraulica",
+    tipo: "recomendado",
+    destaque: true,
+    imagem: "images/produtos/Alicatechave Curva Para Instação De Torneiras Encanador.jpeg",
+    link: "https://meli.la/15K2b6u"
+},
+{
+    nome: "Alicate Bomba D Agua 250mm 10 Gedore",
+    preco: 78.00,
+    categoria: "hidraulica",
+    tipo: "vendido",
+    destaque: true,
+    imagem: "images/produtos/Alicate Bomba D Agua 250mm 10 Gedore.jpeg",
+    link: "https://meli.la/2hCx1Kn"
+},
+
+{
+    nome: "Carrinho De Mão Extra Forte 65 Litros 77714435 Tramontina Cor Cinza Claro",
+    preco: 385.19,
+    categoria: "construcao",
+    tipo: "oferta",
+    destaque: true,
+    imagem: "images/produtos/Carrinho De Mão Extra Forte 65 Litros 77714435 Tramontina Cor Cinza Claro.jpeg",
+    link: "https://meli.la/1mLXYyf"
+},
+
+{
+    nome: "Nível a Laser Verde 2 Linhas Com Tripé Para Profissional Autonivelante",
+    preco: 126.07,
+    categoria: "construcao",
+    tipo: "oferta",
+    destaque: true,
+    imagem: "images/produtos/Nível a Laser Verde 2 Linhas Com Tripé Para Profissional Autonivelante.jpeg",
+    link: "https://meli.la/2b9UQcS"
+},
+
+{
+    nome: "Martelete Rompedor Perfurador Profissional Sds-plus 1100w",
+    preco: 367.90,
+    categoria: "construcao",
+    tipo: "oferta",
+    destaque: true,
+    imagem: "images/produtos/Martelete Rompedor Perfurador Profissional Sds-plus 1100w.jpeg",
+    link: "https://meli.la/1oee5t8"
+},
+
+{
+    nome: "Pneu Carrinho De Mão Não Fura Maciço Completo Universal Fb",
+    preco: 72.40,
+    categoria: "construcao",
+    tipo: "oferta",
+    destaque: true,
+    imagem: "images/produtos/Pneu Carrinho De Mão Não Fura Maciço Completo  Universal Fb.jpeg",
+    link: "https://meli.la/2QL9dkX"
+},
+
+{
+    nome: "Desempenadeira Aço Dentada Com Regulagem 430mm X 120mm",
+    preco: 55.90,
+    categoria: "construcao",
+    tipo: "oferta",
+    destaque: true,
+    imagem: "images/produtos/Desempenadeira Aço Dentada Com Regulagem 430mm X 120mm.jpeg",
+    link: "https://meli.la/18DezuE"
+},
+
+{
+    nome: "Alicate Torquês Turquesa Armador Profissional 12 Aço",
+    preco: 51.31,
+    categoria: "construcao",
+    tipo: "oferta",
+    destaque: true,
+    imagem: "images/produtos/Alicate Torquês Turquesa Armador Profissional 12 Aço.jpeg",
+    link: "https://meli.la/2dK8zdd"
+},
+
+]
+
+// =========================
+// ELEMENTOS DA PÁGINA
+// =========================
+
+const listaProdutos =
+document.getElementById("lista-produtos");
+
+const listaVendidos =
+document.getElementById("lista-vendidos");
+
+const listaRecomendados =
+document.getElementById("lista-recomendados");
+
+// =========================
+// CRIAR CARD
+// =========================
+
+function criarCard(produto){
+
+return `
+
+<div class="produto">
+
+${produto.destaque
+? '<div class="selo">🔥 OFERTA</div>'
+: ''
+}
+
+<img
+src="${produto.imagem}"
+alt="${produto.nome}"
+>
+
+<h3>${produto.nome}</h3>
+
+<p class="preco">
+R$ ${produto.preco.toFixed(2)}
+</p>
+
+<a
+href="${produto.link}"
+target="_blank">
+
+Ver Oferta
+
+</a>
+
+</div>
+
+`;
+
+}
+
+// =========================
+// EXIBIR PRODUTOS
+// =========================
+
+function exibirProdutos(categoria = "todos"){
+
+listaProdutos.innerHTML = "";
+listaVendidos.innerHTML = "";
+listaRecomendados.innerHTML = "";
+
+let produtosFiltrados =
+categoria === "todos"
+? produtos
+: produtos.filter(
+produto =>
+produto.categoria === categoria
+);
+
+produtosFiltrados.forEach(produto => {
+
+if(produto.tipo === "oferta"){
+
+listaProdutos.innerHTML +=
+criarCard(produto);
+
+}
+
+if(produto.tipo === "vendido"){
+
+listaVendidos.innerHTML +=
+criarCard(produto);
+
+}
+
+if(produto.tipo === "recomendado"){
+
+listaRecomendados.innerHTML +=
+criarCard(produto);
+
+}
+
+});
+
+}
+
+// =========================
+// FILTRO CATEGORIA
+// =========================
+
+function filtrarCategoria(categoria){
+
+exibirProdutos(categoria);
+
+}
+
+// =========================
+// INICIAR
+// =========================
+
+exibirProdutos();
